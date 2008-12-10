e632299fc47d4b90648396bfbc3516b797ea50f7
rockitbaby <babe@rockitbaby.de>
2008-12-10 15:21:07 +0100

    a drity hack in init.js to view all of the examples fullscreen on iPhone this makes testing a lot easier.

--- examples/init.js
+++ examples/init.js
@@ -3,10 +3,19 @@
  * in your page and loads each script in the target canvas with the proper id.
  * It is useful to smooth the process of adding Processing code in your page and starting
  * the Processing.js engine.
+ *
+ * this is hacked
+ * to view all examples in iphone, without modifying the html and processing sources
+ * - which only processes the first <script type="application/processing" tag.
+ * - sets iphone specific meta tags
+ * - sets the canvas size to 320 * 460
+ * - set margins to zero
  */
 
 if ( window.addEventListener ) {
 	window.addEventListener("load", function() {
+	
+		var allCanvas = new Array();
 		var scripts = document.getElementsByTagName("script");
 		
 		for ( var i = 0; i < scripts.length; i++ ) {
@@ -21,9 +30,33 @@ if ( window.addEventListener ) {
 				}
 
 				if ( canvas ) {
-					Processing(canvas, scripts[i].text);
+					p = Processing(canvas, scripts[i].text);
+					//break only process the first script tag in a page
+					break;
 				}
 			}
 		}
+		if (canvas) {
+			var body = document.getElementsByTagName('body')[0];
+			body.innerHTML = '';
+			body.appendChild(canvas);
+			body.style.margin = '0';
+			canvas.style.margin = '0';
+			
+			var head = document.getElementsByTagName('head')[0];
+			var metaViewport = document.createElement('meta');
+			metaViewport.setAttribute("name", "viewport");
+			metaViewport.setAttribute("content", "width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;");
+	
+			var metaCapable = document.createElement('meta');
+			metaCapable.setAttribute("name", "apple-mobile-web-app-capable");
+			metaCapable.setAttribute("content", "yes");
+			
+			head.appendChild(metaViewport);
+			head.appendChild(metaCapable);
+			
+			p.size(320, 460);
+		}
+		
 	}, false);
 }
